935d681aa6193442433aecd81fb41b0aaa4946e7
Carlos Velasco Nino <cvelnin@gmail.com>
2020-08-22 13:16:38 +0200

    Use Spring for injecting dependencies

--- src/main/java/advisor/AppConfig.java
+++ src/main/java/advisor/AppConfig.java
@@ -1,17 +1,26 @@
 package advisor;
 
+import advisor.authentication.*;
+import advisor.controller.CommandLineController;
+import advisor.model.service.Advisor;
+import advisor.model.service.SpotifyAdvisor;
+import advisor.view.CommandLineView;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import java.io.PrintStream;
 import java.util.Scanner;
 
+import static advisor.authentication.SpotifyAccessCodeFetcher.ACCESS_CODE_SERVER_TIMEOUT_SECONDS;
+
 @Configuration
-@PropertySource("application.properties")
 @ComponentScan("advisor")
+@RequiredArgsConstructor
 public class AppConfig {
+
+    private final AdvisorProperties advisorProperties;
 
     @Bean
     Scanner scanner() {
@@ -21,5 +30,55 @@ public class AppConfig {
     @Bean
     PrintStream printStream() {
         return new PrintStream(System.out);
+    }
+
+    @Bean
+    CommandLineView commandLineView() {
+        return new CommandLineView(scanner(), printStream(), advisorProperties.getPageSize());
+    }
+
+    @Bean
+    SpotifyAccessCodeFetcher spotifyAccessCodeFetcher() {
+        return new SpotifyAccessCodeFetcher(
+                advisorProperties.getSpotifyAccessHost(),
+                advisorProperties.getSpotifyClientId(),
+                advisorProperties.getRedirectUri(),
+                commandLineView(),
+                ACCESS_CODE_SERVER_TIMEOUT_SECONDS);
+    }
+
+    @Bean
+    SpotifyAccessTokenFetcher spotifyAccessTokenFetcher() {
+        return new SpotifyAccessTokenFetcher(
+                advisorProperties.getSpotifyAccessHost(),
+                advisorProperties.getSpotifyClientId(),
+                advisorProperties.getSpotifyClientSecret(),
+                advisorProperties.getRedirectUri(),
+                commandLineView());
+    }
+
+    @Bean
+    UserCommandAuthentication userCommandAuthentication() {
+        return new SpotifyOAuthUserCommandAuthentication(
+                spotifyAccessCodeFetcher(),
+                spotifyAccessTokenFetcher(),
+                commandLineView());
+    }
+
+    @Bean
+    CommandLineController commandLineController() {
+        return new CommandLineController(
+                commandLineView(),
+                advisor(),
+                userCommandAuthentication(),
+                advisorProperties.getPageSize());
+    }
+
+    @Bean
+    Advisor advisor() {
+        return new SpotifyAdvisor(
+                advisorProperties.getSpotifyResourceHost(),
+                new UserCommandAuthenticationFacade(userCommandAuthentication()),
+                advisorProperties.getPageSize());
     }
 }
